674c7f161a96762839cd6684c94af39b8b9d292e
jifsvbi <joaopero222931@gmail.com>
2025-02-25 10:07:22 -0300

    arrumado feito e terminado

--- Aula 3/user.js
+++ Aula 3/user.js
@@ -1,9 +1,15 @@
 class User{
-    constructor(id, nome, email){
-        this.id = id; // id do usuario
-        this.nome = nome; // nome do usuario
-        this.email = email; // email do usuario
+    constructor(id,nome,email){
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+}
+class Admin extends User{
+    constructor(id,nome,email,nivelAcesso){
+        super(id,nome,email)
+        this.nivelAcesso = nivelAcesso
     }
 }
 
-module.exports = User; // exportar o modulo
+module.exports = User //Exporta a classe User
--- Aula 3/userService.js
+++ Aula 3/userService.js
@@ -1,14 +1,35 @@
-const User = require("./user");
-const path = require('path');
-const fs = require('fs');
+const User = require('./user');
+const paht = require('path'); // modulo para manipular caminhos
+const fs = require('fs'); // modulo para manipular arquivos
 
-class userService{
+class userService {
     constructor(){
-        this.Users = []; //Array para armazenar user
-        
-        this.nextid = 1; //contador para gerar id
-        
+        this.filePath = path.join(__dirname, 'user.json');
+        this.users = [];//Array para armazenar user
+        this.nextid = 1;//icontador para gerar id
     }
+
+    loadUsers(){
+        try{
+        if(fs.existsSync(this.filePath)){//verifica se o arquivo existe
+            const data = fs.readFileSync(this.filePath);//le o arquivo
+            return JSON.parse(data);//transforma json em objeto
+        }
+    }catch(erro){
+        console.log("Erro ao carregar o arquivo", erro)
+    }
+    return[]
+}
+
+getNextId(){
+    try{
+    if(this.users.length === 0) return ;
+    return Math.max(...this.users.map(user => user.id))+1
+}catch (erro){
+    console.log('Erro ao buscar o id', erro)
+}
+}
+
 
     addUser(nome, email){
         const user = new User(this.nextid++, nome, email);
